56d728ca5bd6dc5038abcd313a552a6d4d5176da
Emil Bruckner <emil.bruckner@gmail.com>
2018-07-24 18:19:29 +0200

    init

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import animationData from './data-step1.json'
+import lottie from 'lottie-web'
+
+class IndexPage extends React.Component {
+  componentDidMount() {
+    lottie.loadAnimation({
+      container: document.getElementById('lottie'), // the dom element that will contain the animation
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData
+    })
+  }
+
+  render() {
+    return <div id="lottie">hi</div>
+  }
+}
+
+export default IndexPage
